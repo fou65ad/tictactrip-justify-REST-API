64c92727a544279469d10cf349ef6968aba7f144
fouad <bouarourou.fouad@gmail.com>
2018-11-10 22:08:59 +0100

    make it better

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
-const client = require('redis').createClient();
-const rateLimiter = require('redis-express-rate-limiter')
+
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
@@ -11,25 +10,5 @@ var AuthController = require('./auth/AuthController');
 app.use('/api', AuthController);
 
 
-
-
-// const limiterConfigs = {
-//   path: '/api/auth/justify',
-//   method: 'post',
-//   limitBy: 'body.token',
-//   // 50 requests per hour
-//   total: 3,
-//   // time is in milliseconds
-//   expiresIn: 1000 * 60 ,
-//   decrementAmount: (req, redisInfo) => 1,
-//   onRateLimited: (req, res, next) => res.sendStatus(429),
-//   onError: (err, req, res, next) => res.sendStatus(500),
-//   onPassThrough: res => res.sendStatus(200)
-// }
-// app.use(rateLimiter(limiterConfigs, client)) //you can also use many limiters each for a different route!
-
-// app.post('/api/auth/justify', (req, res) => {
-//   res.sendStatus(200)
-// })
 
 module.exports = app;
