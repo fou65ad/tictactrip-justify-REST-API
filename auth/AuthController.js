49d5ef19ba4ed810628d8ebe6b95191dd4de84e6
fouad <bouarourou.fouad@gmail.com>
2018-11-10 22:28:29 +0100

    make it better3

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -13,8 +13,8 @@ var config = require('../config');
 var VerifyToken = require('./VerifyToken');
 var justification = require('./justification');
 
-var FastRateLimit = require("./fast-rate-limit.js").FastRateLimit;
-var decr = require("./fast-rate-limit.js");
+var FastRateLimit = require("./fast-rate-limit").FastRateLimit;
+var decr = require("./fast-rate-limit");
 
  
 var messageLimiter = new FastRateLimit({
